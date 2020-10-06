d7f19eb1b57f4e0f3825e5c891d36b8e5c98943d
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-06 15:37:19 +0200

    updateUI created random coloro created action defined

--- FourColorBoxes/ViewController.swift
+++ FourColorBoxes/ViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var clockWise = true
-    
+    // MARK: Outlets
     @IBOutlet weak var upLeftButton: UIButton!
     @IBOutlet weak var upRightButton: UIButton!
     @IBOutlet weak var downLeftButton: UIButton!
@@ -18,21 +17,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var textLable: UILabel!
     @IBOutlet weak var changeDireccionButton: UIButton!
     
+    // MARK: State
+    var clockWise = true
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
+    
+    // MARK: Helpers
+    
+    func updateUI() {
+        if clockWise {
+            textLable.text = "Go clockwise !!"
+        } else {
+            textLable.text = "Go counterwise !!"
+        }
+    }
+    
+    func getRandomColor() -> UIColor {
+        //Generate between 0 to 1
+        let red:CGFloat = CGFloat(drand48())
+        let green:CGFloat = CGFloat(drand48())
+        let blue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    // MARK: Action
+    
     @IBAction func didTappedUpLeftButton(_ sender: UIButton) {
+        if clockWise {
+            upRightButton.backgroundColor = getRandomColor()
+        } else {
+            downLeftButton.backgroundColor = getRandomColor()
+        }
     }
     @IBAction func didTappedUpRightButton(_ sender: UIButton) {
+        if clockWise {
+            downRightButton.backgroundColor = getRandomColor()
+        } else {
+            upLeftButton.backgroundColor = getRandomColor()
+        }
     }
     @IBAction func didTappedDownLeftButton(_ sender: UIButton) {
+        if clockWise {
+            upLeftButton.backgroundColor = getRandomColor()
+        } else {
+            downRightButton.backgroundColor = getRandomColor()
+        }
     }
     @IBAction func didTappedDownRightButton(_ sender: UIButton) {
+        if clockWise {
+            downLeftButton.backgroundColor = getRandomColor()
+        } else {
+            upRightButton.backgroundColor = getRandomColor()
+        }
     }
     @IBAction func didTappedChangeDirecction(_ sender: UIButton) {
-        clockWise = false
         print(clockWise)
+        clockWise.toggle()
+        updateUI()
     }
     
 }
