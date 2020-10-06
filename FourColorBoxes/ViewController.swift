70da4feb489923a6ea34673a1ab66944f1b9e7c6
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-06 15:24:04 +0200

    main storyBoard created constrains created buttons and label connected

--- FourColorBoxes/ViewController.swift
+++ FourColorBoxes/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var clockWise = true
+    
+    @IBOutlet weak var upLeftButton: UIButton!
+    @IBOutlet weak var upRightButton: UIButton!
+    @IBOutlet weak var downLeftButton: UIButton!
+    @IBOutlet weak var downRightButton: UIButton!
+    @IBOutlet weak var textLable: UILabel!
+    @IBOutlet weak var changeDireccionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func didTappedUpLeftButton(_ sender: UIButton) {
+    }
+    @IBAction func didTappedUpRightButton(_ sender: UIButton) {
+    }
+    @IBAction func didTappedDownLeftButton(_ sender: UIButton) {
+    }
+    @IBAction func didTappedDownRightButton(_ sender: UIButton) {
+    }
+    @IBAction func didTappedChangeDirecction(_ sender: UIButton) {
+        clockWise = false
+        print(clockWise)
+    }
+    
 }
 
